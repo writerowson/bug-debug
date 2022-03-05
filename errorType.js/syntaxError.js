56019983218a837ed927eccfd7ae6f52295cdffe
coderowson <runurowson@gmail.com>
2022-03-05 22:00:51 +0600

    hoyna

---
+++ errorType.js/syntaxError.js
@@ -0,0 +1,7 @@
+// There are three types of errors in programming: (a) Syntax Errors, (b) Runtime Errors, and (c) Logical Errors.
+
+{/* <script type="text/javascript">
+
+</script> */}
+
+console.log('hero')
